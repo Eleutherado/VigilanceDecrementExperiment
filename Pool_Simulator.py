3149cc8dd2f601da9814c615d888a741991b3814
Eleutherado <emiliov432@gmail.com>
2017-10-29 22:04:31 -0400

    between swimmer collisions, has vibrating bug

--- Pool_Simulator.py
+++ Pool_Simulator.py
@@ -10,24 +10,35 @@ POOL_BORDER_WIDTH = WIDTH/20
 POOL_BORDER_HEIGHT = HEIGHT/20
 NUM_SWIMMERS = 20
 
+MIN_SPEED = 1
+MAX_SPEED = 5
+
 # Team blopit is FIRE & INCLUSIVE TO ALL GENDERS, RACES, SEXUALITIES, SCPECIES and ABILITIES <3
+
+def getNewSpeed():
+    return random.randint(MIN_SPEED, MAX_SPEED)
 
 ####################################
 # CLASSES
 ####################################
 
+
 class MovingDot(object):
     dotCount = 0
     dirs = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
     dirNums = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
+    maxSpeed = 5
+    minSpeed = 1
 
     def __init__(self, x, y):
         MovingDot.dotCount += 1
-        #MOTION
+        #PLACEMENT
         self.x = x
         self.y = y
         self.r = 15
-        self.speed = random.randint(2, 5) # random speed
+
+        #MOTION
+        self.speed =  getNewSpeed()
         self.dir = random.randint(0,7) # random direction 
         self.moveX = MovingDot.dirNums[self.dir][0] * self.speed
         self.moveY = MovingDot.dirNums[self.dir][1] * self.speed
@@ -54,22 +65,24 @@ class MovingDot(object):
         d = ((self.x - x)**2 + (self.y - y)**2)**0.5
         return (d <= self.r)
 
-    def draw(self, canvas):
-        leftX = self.x-self.r
-        rightX = self.x+self.r
-        topY = self.y-self.r
-        bottomY = self.y+self.r
+    def draw(self, canvas): 
+        leftX = self.x - self.r
+        rightX = self.x + self.r
+        topY = self.y - self.r
+        bottomY = self.y + self.r
+
         bodyWidth = self.r * 2
         eyeWidthOffset = bodyWidth/5 # eyes take up a fifth of the face, and are a fifth from the edges
+
         #make above the eyes
         submergeY = topY + self.r/3 # y coordinte of water line when submerged
         floatY = bottomY - self.r/3 #y coordinte of water line when floating
 
         #create body
-        canvas.create_oval(leftX, topY,
+        canvas.create_oval(leftX,topY,
                             rightX, bottomY,
                             fill=self.bodyFill)
-        #create left eye -- Eyes go
+        #create left eye 
         canvas.create_oval(leftX + eyeWidthOffset, topY + bodyWidth/4,
                             leftX + 2 * eyeWidthOffset, topY + bodyWidth/2,
                             fill=self.featureFill)
@@ -85,24 +98,24 @@ class MovingDot(object):
 
 
     def onTimerFired(self, data):
-        #check for pool border COLLISIONS
-        # Need to check for corner collisions!!!
+
         self.checkWallCollisions()
-        self.checkSwimmerCollisions()
+        self.checkSwimmerCollisions(data)
 
         self.move()
 
-
-        #CHECK SWIMMER COLLISIONS
 
     def move(self):
         #check for direction and modify x and y coords accordingly
         self.x += self.moveX
         self.y += self.moveY
+        
 
-    def updateDir(self):
+    def updateDir(self, newDir):
+        self.dir = newDir
         self.moveX = MovingDot.dirNums[self.dir][0] * self.speed
         self.moveY = MovingDot.dirNums[self.dir][1] * self.speed
+
 
     def checkWallCollisions(self):
         collidedRight = self.x + self.r >= WIDTH - POOL_BORDER_WIDTH
@@ -111,40 +124,56 @@ class MovingDot(object):
         collidedBottom = self.y + self.r >= HEIGHT - POOL_BORDER_HEIGHT
 
         if(not (collidedLeft or collidedRight) and collidedTop): # COLLIDED with top
-            self.dir = random.randint(3, 5)
-            self.updateDir()
+            newDir = random.randint(3, 5)
+            self.updateDir(newDir)
 
         elif(collidedRight and collidedTop): # COLLIDED with top right
-            self.dir = random.randint(4, 6)
-            self.updateDir()
+            newDir = random.randint(4, 6)
+            self.updateDir(newDir)
 
         elif(collidedRight and not (collidedTop or collidedBottom)): # COLLIDED with right 
-            self.dir = random.randint(5, 7)
-            self.updateDir()
+            newDir = random.randint(5, 7)
+            self.updateDir(newDir)
 
         elif(collidedRight and collidedBottom): # COLLIDED with bottom right 
-            self.dir = random.choice([6, 7, 0])
-            self.updateDir()
+            newDir= random.choice([6, 7, 0])
+            self.updateDir(newDir)
 
         elif(not (collidedRight or collidedLeft) and collidedBottom): # COLLIDED with bottom 
-            self.dir = random.choice([7, 0, 1])
-            self.updateDir()
+            newDir = random.choice([7, 0, 1])
+            self.updateDir(newDir)
 
         elif(collidedLeft and collidedBottom): # COLLIDED with bottom left
-            self.dir = random.randint(0, 2)
-            self.updateDir() 
+            newDir = random.randint(0, 2)
+            self.updateDir(newDir) 
 
         elif(collidedLeft and not (collidedTop or collidedBottom)): # COLLIDED with left
-            self.dir = random.randint(1, 3)
-            self.updateDir()
+            newDir = random.randint(1, 3)
+            self.updateDir(newDir)
 
         elif(collidedLeft and collidedBottom): # COLLIDED with top left
-            self.dir = random.randint(2, 4)
-            self.updateDir()
+            newDir = random.randint(2, 4)
+            self.updateDir(newDir)
 
 
-    def checkSwimmerCollisions(self):
-        pass
+    def checkSwimmerCollisions(self, data): # TODO: MAKE SURE SWIMMER NOT COLLIDING WITH WALL TOO
+        for other in data.dots:
+            if(other is self): # always colliding with self
+                break
+
+            xDist = abs(self.x - other.x)
+            yDist = abs(self.y - other.y)
+            collided = (xDist**2 + yDist**2)**0.5 < (self.r * 2) #Pythagoras, checks that radii are touching, add 2 for legroom
+
+            if (collided): #collided N
+                oppositeDirSelf = (self.dir + 4) % 8 
+                self.updateDir(oppositeDirSelf)
+                self.speed = getNewSpeed()
+
+                oppositeDirOther = (other.dir + 4) % 8
+                other.updateDir(oppositeDirOther)   
+                other.speed = getNewSpeed()
+
 
         
 
@@ -164,7 +193,6 @@ def init(data):
         yCord = random.randint(50+cR,data.height-50-cR)
         data.dots.append(MovingDot(xCord, yCord))
         i = i + 1
-
 def mousePressed(event, data):
     for dot in reversed(data.dots):
         if (dot.containsPoint(event.x, event.y)):
